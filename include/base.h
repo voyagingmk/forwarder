be6e2316f47364fd7470636405e13613733e42be
Wyman-home-pc <voyaingmk@gmail.com>
2016-10-27 20:09:26 +0800

    no message

--- include/base.h
+++ include/base.h
@@ -25,5 +25,20 @@
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
 
+#define ASIO_STANDALONE
+
+#ifdef _MSC_VER
+#define ASIO_MSVC _MSC_VER
+#define _WEBSOCKETPP_CPP11_STL_
+#define _WEBSOCKETPP_CPP11_THREAD_
+#define _WEBSOCKETPP_CPP11_FUNCTIONAL_
+#define _WEBSOCKETPP_CPP11_SYSTEM_ERROR_
+#define _WEBSOCKETPP_CPP11_RANDOM_DEVICE_
+#define _WEBSOCKETPP_CPP11_MEMORY_
+#endif
+
+#include <websocketpp/server.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
+
 
 #endif
