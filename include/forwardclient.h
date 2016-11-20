6b94c25a5a687fd5687ed0acb6bebe859e1d2207
Wyman-home-pc <voyaingmk@gmail.com>
2016-11-20 21:26:33 +0800

    no message

--- include/forwardclient.h
+++ include/forwardclient.h
@@ -4,38 +4,43 @@
 #include "base.h"
 #include "uniqid.h"
 
-class ForwardClient {
-public:
-	inline UniqID getUniqID() const {
-		return id;
-	}
-	virtual void hh() const {
-	}
-public:
-	UniqID id = 0;
-	uint32_t ip = 0;
-};
 
-class ForwardClientENet: public ForwardClient {
-public:
-	ForwardClientENet():
-		peer(nullptr)
-	{};
-	ForwardClientENet(const ForwardClientENet& x) = delete;
-	ForwardClientENet& operator=(const ForwardClientENet& x) = delete;
-public:
-	ENetPeer* peer;
-};
+namespace forwarder {
 
-class ForwardClientWS : public ForwardClient {
-	typedef websocketpp::connection_hdl Hdl;
-public:
-	ForwardClientWS()
-	{};
-	ForwardClientWS(const ForwardClientWS& x) = delete;
-	ForwardClientWS& operator=(const ForwardClientWS& x) = delete;
-public:
-	Hdl hdl;
+	class ForwardClient {
+	public:
+		inline UniqID getUniqID() const {
+			return id;
+		}
+		virtual void hh() const {
+		}
+	public:
+		UniqID id = 0;
+		uint32_t ip = 0;
+	};
+
+	class ForwardClientENet : public ForwardClient {
+	public:
+		ForwardClientENet() :
+			peer(nullptr)
+		{};
+		ForwardClientENet(const ForwardClientENet& x) = delete;
+		ForwardClientENet& operator=(const ForwardClientENet& x) = delete;
+	public:
+		ENetPeer* peer;
+	};
+
+	class ForwardClientWS : public ForwardClient {
+		typedef websocketpp::connection_hdl Hdl;
+	public:
+		ForwardClientWS()
+		{};
+		ForwardClientWS(const ForwardClientWS& x) = delete;
+		ForwardClientWS& operator=(const ForwardClientWS& x) = delete;
+	public:
+		Hdl hdl;
+	};
+
 };
 
 #endif
